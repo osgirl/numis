1dd922f95f2cbd6ce6e7a8dd3259bdb1742cccbf
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2014-12-02 10:01:49 +0100

    Deshabitamos las opciones de redes sociales
    Se deshabilita el registro, login y relación del perfil de usuario con
    redes sociales.

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 	// Setting up the users profile api
 	app.route('/users/me').get(users.me);
 	app.route('/users').put(users.update);
-	app.route('/users/accounts').delete(users.removeOAuthProvider);
+//	app.route('/users/accounts').delete(users.removeOAuthProvider);
 
 	// Setting up the users password api
 	app.route('/users/password').post(users.changePassword);
@@ -25,6 +25,7 @@ module.exports = function(app) {
 	app.route('/auth/signin').post(users.signin);
 	app.route('/auth/signout').get(users.signout);
 
+/*
 	// Setting the facebook oauth routes
 	app.route('/auth/facebook').get(passport.authenticate('facebook', {
 		scope: ['email']
@@ -51,6 +52,7 @@ module.exports = function(app) {
 	// Setting the github oauth routes
 	app.route('/auth/github').get(passport.authenticate('github'));
 	app.route('/auth/github/callback').get(users.oauthCallback('github'));
+*/
 
 	// Finish by binding the user middleware
 	app.param('userId', users.userByID);
