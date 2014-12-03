80a3ed1bd345516d8a9e86e9d4d015f832d0b5ef
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2014-12-03 09:46:35 +0100

    i18n for Groupbuy menus
    Internacionalización del menú de Compras en Grupo

--- public/modules/groupbuys/config/groupbuys.client.config.js
+++ public/modules/groupbuys/config/groupbuys.client.config.js
@@ -1,11 +1,13 @@
 'use strict';
 
 // Configuring the Articles module
-angular.module('groupbuys').run(['Menus',
-	function(Menus) {
+angular.module('groupbuys').run(['Menus', '$translate',
+	function(Menus, $translate) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Groupbuys', 'groupbuys', 'dropdown', '/groupbuys(/create)?');
-		Menus.addSubMenuItem('topbar', 'groupbuys', 'List Groupbuys', 'groupbuys');
-		Menus.addSubMenuItem('topbar', 'groupbuys', 'New Groupbuy', 'groupbuys/create');
+		$translate(['groupbuys.Groupbuys', 'groupbuys.List_Groupbuys', 'groupbuys.New_Groupbuy']).then(function (translations) {
+			Menus.addMenuItem('topbar', translations['groupbuys.Groupbuys'], 'groupbuys', 'dropdown', '/groupbuys(/create)?');
+			Menus.addSubMenuItem('topbar', 'groupbuys', translations['groupbuys.List_Groupbuys'], 'groupbuys');
+			Menus.addSubMenuItem('topbar', 'groupbuys', translations['groupbuys.New_Groupbuy'], 'groupbuys/create');
+		});
 	}
 ]);
