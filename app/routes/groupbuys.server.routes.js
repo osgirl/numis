dd8ded03f7f094846e92334d614c7e2df31f1fc6
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2014-12-14 12:40:54 +0100

    Change users and groupbuys routes
    Change users and groupbuys routes to use mongoose-rest-endpoints module.

--- app/routes/groupbuys.server.routes.js
+++ app/routes/groupbuys.server.routes.js
@@ -1,20 +1,58 @@
 'use strict';
 
+var mongoose = require('mongoose'),
+	restEndpoints = require('mongoose-rest-endpoints'),
+	users = require('../../app/controllers/users.server.controller');
+
 module.exports = function(app) {
-	var users = require('../../app/controllers/users.server.controller');
-	var groupbuys = require('../../app/controllers/groupbuys.server.controller');
 
-	// Groupbuys Routes
-	app.route('/groupbuys')
-		.get(groupbuys.list)
-		.post(users.requiresLogin, groupbuys.create);
+// TODO: Comment it!
+//	mongooseRestEndpoints.log.verbose(true);
 
-	app.route('/groupbuys/:groupbuySlug')
-		.get(groupbuys.read)
-		.put(users.requiresLogin, groupbuys.hasAuthorization, groupbuys.update)
-		.delete(users.requiresLogin, groupbuys.hasAuthorization, groupbuys.delete);
+	// Register end point for '/users' and /users/:id'
+	new restEndpoints
+				.endpoint('/api/v1/groupbuys', 'Groupbuy', {
+					limitFields: ['_id', 'name', 'description', 'slug', 'status', 'managers', 'members'],
+					populate: ['members', 'managers', 'user'],
+					queryParams: ['$in_status', 'name', '$gte_created'],
+					pagination: {
+						perPage: 20,
+						sortField: 'name'
 
-	// Finish by binding the Groupbuy middleware
-	app.param('groupbuyId', groupbuys.groupbuyByID);
-	app.param('groupbuySlug', groupbuys.groupbuyBySlug);
+					}
+				})
+				.addMiddleware('*', users.requiresLogin)
+				.tap('pre_filter', 'list', function(req, data, next) {
+					var isAdmin = (req.user && req.user.roles && req.user.roles.indexOf('admin') !== -1);
+
+					if (!isAdmin) {
+						if (req.user) {
+	//									data.$or = [{_id: '547e3ab9b1a7a0ad7df3fcac'}, {name: 'Japón-500 yenes - Serie prefecturas (Septiembre 2014)'}];
+							data.$or = [{status: { '$in': ['new', 'published', 'payments', 'paid', 'shipments', 'closed']} },
+										{managers: {$elemMatch: {managers: req.user._id} } }];
+						} else {
+							data.status = { '$in': ['new', 'published', 'payments', 'paid', 'shipments', 'closed']};
+						}
+
+					}
+					return next(data);
+				})
+
+				.tap('pre_response', 'list', function(req, data, next) {
+					var isAdmin = (req.user && req.user.roles && req.user.roles.indexOf('admin') !== -1),
+						isManager = false,
+						isMember = false;
+
+					return next(data);
+				})
+				.tap('pre_response', 'fetch', function(req, data, next) {
+					var isAdmin = (req.user && req.user.roles && req.user.roles.indexOf('admin') !== -1),
+						isManager = false,
+						isMember = false;
+
+					return next(data);
+				})
+
+				.register(app);
+
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -3,16 +3,136 @@
 /**
  * Module dependencies.
  */
-var passport = require('passport');
+var passport = require('passport'),
+	mongoose = require('mongoose'),
+	restEndpoints = require('mongoose-rest-endpoints'),
+	errorHandler = require('../../app/controllers/errors.server.controller'),
+	users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
 	// User Routes
-	var users = require('../../app/controllers/users.server.controller');
+	/*
+	 *
+	 */
+	var prepareFields = function(data, showProtected) {
+		// Rename _id field
+		data.id = data._id;
+		delete data._id;
+
+		// Add links field.
+		data.links = [
+			{ref: 'self', href: ''},
+			{ref: 'user', href: ''},
+			{ref: 'avatar', href: ''}
+		];
+
+		// Delete private fields fromresponse
+		delete data.__v;
+		delete data.password;
+		delete data.salt;
+		delete data.displayName;
+		delete data.avatar;
+
+		// Delete protected fields from response
+		if (!showProtected) {
+			delete data.lastName;
+			delete data.firstName;
+			delete data.homeAddress;
+			delete data.roles;
+			delete data.email;
+			delete data.provider;
+		}
+	};
+
+	/*
+	*
+	*/
+	var listPreFilter = function(req, query, next) {
+
+//console.log('LUIS req: ', req.query);
+
+		next(query);
+	};
+
+
+	/*
+	 *
+	 */
+	var fetchPreResponse = function(req, data, next) {
+		var isAdmin = (req.user && req.user.roles && req.user.roles.indexOf('admin') !== -1),
+			isMe    = (req.user && req.user._id.equals(data._id) );
+
+		prepareFields(data, isAdmin || isMe);
+
+		next(data);
+	};
+
+	/*
+	 *
+	 */
+	var listPreResponse = function(req, data, next) {
+		var isAdmin = (req.user && req.user.roles && req.user.roles.indexOf('admin') !== -1);
+
+		for (var i = 0; i < data.length; i++) {
+			prepareFields(data[i], isAdmin);
+		}
+
+		next(data);
+	};
+
+	/*
+	 *
+	 */
+	var preResponseError = function(req, error, next) {
+		if (error && !error.message)
+			error.message = errorHandler.getErrorMessage(error);
+
+		next(error);
+	};
+
+// TODO: Comment it!
+	//restEndpoints.log.verbose(true);
+
+	// Register end point for '/users' and /users/:id'
+	new restEndpoints
+				.endpoint('/api/v1/users', 'User', {
+					limitFields: ['_id', 'username', 'slug', 'roles'],
+					queryParams: ['username', 'slug', 'email', '$in_roles'],
+					pagination: {
+						perPage: 50,
+						sortField: 'username'
+					}
+				})
+				.addMiddleware('*', users.requiresLogin)
+				.tap('pre_filter', 'list', listPreFilter)
+				.tap('pre_filter', 'fetch', listPreFilter)
+				.tap('post_retrieve', 'list', listPreFilter)
+				.tap('post_retrieve', 'fetch', listPreFilter)
+				.tap('pre_response', 'list', listPreResponse)
+				.tap('pre_response', 'fetch', fetchPreResponse)
+				.tap('pre_response_error', 'fetch', preResponseError)
+				.tap('pre_response_error', 'list', preResponseError)
+				.tap('pre_response_error', 'post', preResponseError)
+				.tap('pre_response_error', 'put', preResponseError)
+				.tap('pre_response_error', 'delete', preResponseError)
+				.register(app);
+
+
 
 	// Setting up the users profile api
 	app.route('/users/me').get(users.me);
-	app.route('/users').put(users.update);
+	app.route('/users').put( function(req, res, next) {
+		res.url = '/api/v1' + res.url;
+
+		next();
+	});
 //	app.route('/users/accounts').delete(users.removeOAuthProvider);
+
+	// Setting up the users avatar api
+	app.route('/api/v1/users/:userId/avatar')
+		.get(users.requiresLogin, users.getAvatar)
+		.put(users.requiresLogin, users.updateAvatar)
+		.delete(users.requiresLogin, users.deleteAvatar);
 
 	// Setting up the users password api
 	app.route('/users/password').post(users.changePassword);
