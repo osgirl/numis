89efb54856ba5492ac72bac40ae19f36dbf3d4e8
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2015-01-17 16:28:37 +0100

    Create root endpoint for API v1

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -9,3 +9,63 @@ exports.index = function(req, res) {
 		request: req
 	});
 };
+
+/**
+ * Get root point for ReST API
+ */
+exports.getApiRootPoint = function(req, res) {
+	var user 		= req.user,
+		apiVersion	= req.apiVersion,
+		isAdmin 	= (user && user.roles && user.roles.indexOf('admin') !== -1),
+		response	= {};
+
+	// Prepare response in JSON+HAL format.
+	switch(apiVersion) {
+		case '1':
+			response = {
+				_links: {
+					self: {
+						href: '/api/v1/'
+					},
+					lastApiVersion: {
+						href: '/api/v1/'
+					},
+					curies: [
+						{
+							name: 'ht',
+							href: '/api/v1/rels/{rel}',
+							templated: true
+						}
+					],
+					'ht:users': {
+						href: '/api/v1/users'
+					},
+					'ht:me': {
+						href: '/api/v1/users/' + user._id
+					},
+					'ht:groupbuys': {
+						href: '/api/v1/groupbuys'
+					}
+				}
+			};
+			break;
+		default: {
+			response = {
+				_links: {
+					lastApiVersion: {
+						href: '/api/v1/'
+					}
+				}
+			};
+		}
+
+	}
+
+	res.jsonp(response);
+};
+
+
+exports.versionByNumber = function(req, res, next, apiVersion) {
+	req.apiVersion = apiVersion;
+	next();
+};
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
 	app.route('/').get(core.index);
+
+	// ReST API Root EndPoint
+	app.route('/api/v:version/').get(core.getApiRootPoint);
+
+	// Finish by binding the Item middleware
+	app.param('version', core.versionByNumber);
 };
