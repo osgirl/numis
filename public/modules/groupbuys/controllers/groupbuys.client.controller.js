33b4f58855f3907795350c2702ef60ff7f36838b
Chema <chemaur@gmail.com>
2015-02-10 21:04:43 +0100

    Function to load the specified tabs for a role

--- public/modules/groupbuys/controllers/groupbuys.client.controller.js
+++ public/modules/groupbuys/controllers/groupbuys.client.controller.js
@@ -194,6 +194,114 @@ function($scope, Restangular, $stateParams, $location, $translate, Authenticatio
 	};
 
 
+	// ----------------------
+
+	/**
+	* @ngdoc method
+	* @name groupbuys.controller:GroupbuysController.$scope.loadTabs
+	* @methodOf groupbuys.controller:GroupbuysController
+	*
+	* @description
+	* Loads the proper tabs in the scope based on the role of the user
+	*/
+	$scope.loadTabs =  function() {
+		// Create the tabs menu according to the permissions of the user:
+		$translate([
+			'groupbuys.Information',
+			'groupbuys.Items',
+			'groupbuys.Buyers',
+			'groupbuys.Requests',
+			'groupbuys.Requests_summary',
+			'groupbuys.Messaging',
+			'groupbuys.Payments',
+			'groupbuys.Deliveries',
+			'groupbuys.Managers',
+			'groupbuys.Configuration'
+			]).then(function (translations) {
+				switch ( $scope.userRole ){
+					case 'manager':
+						$scope.tabs = [
+							{
+								title: translations['groupbuys.Information'],
+								template: '/modules/groupbuys/views/tabs/info-groupbuy.client.view.html',
+								active: true
+							},{
+								title: translations['groupbuys.Items'],
+								template:'/modules/groupbuys/views/tabs/items-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Buyers'],
+								template:'/modules/groupbuys/views/tabs/buyers-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Requests'],
+								template:'/modules/groupbuys/views/tabs/requests-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Requests_summary'],
+								template:'/modules/groupbuys/views/tabs/requests-summary-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Messaging'],
+								template:'/modules/groupbuys/views/tabs/messaging-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Payments'],
+								template:'/modules/groupbuys/views/tabs/payments-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Deliveries'],
+								template:'/modules/groupbuys/views/tabs/deliveries-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Managers'],
+								template:'/modules/groupbuys/views/tabs/managers-groupbuy.client.view.html'
+							},{
+								title: translations['groupbuys.Configuration'],
+								template:'/modules/groupbuys/views/tabs/config-groupbuy.client.view.html'
+							}
+						];
+						break;
+						case 'member':
+							$scope.tabs = [
+								{
+									title: translations['groupbuys.Information'],
+									template: '/modules/groupbuys/views/tabs/info-groupbuy.client.view.html',
+									active: true
+								},{
+									title: translations['groupbuys.Items'],
+									template:'/modules/groupbuys/views/tabs/items-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Buyers'],
+									template:'/modules/groupbuys/views/tabs/buyers-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Messaging'],
+									template:'/modules/groupbuys/views/tabs/messaging-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Payments'],
+									template:'/modules/groupbuys/views/tabs/payments-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Deliveries'],
+									template:'/modules/groupbuys/views/tabs/deliveries-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Managers'],
+									template:'/modules/groupbuys/views/tabs/managers-groupbuy.client.view.html'
+								}
+							];
+							break;
+							default:
+								$scope.tabs = [
+								{
+									title: translations['groupbuys.Information'],
+									template: '/modules/groupbuys/views/tabs/info-groupbuy.client.view.html',
+									active: true
+								},{
+									title: translations['groupbuys.Items'],
+									template:'/modules/groupbuys/views/tabs/items-groupbuy.client.view.html'
+								},{
+									title: translations['groupbuys.Managers'],
+									template:'/modules/groupbuys/views/tabs/managers-groupbuy.client.view.html'
+								}
+								];
+								break;
+				}
+			});
+	};
+
+
 // from the top of the file
 }
 ]);
