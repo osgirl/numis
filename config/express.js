4d723aa964ad534bcc10ac27ac3ec75ad6ded330
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2015-02-20 18:17:57 +0100

    Bump deps

--- config/express.js
+++ config/express.js
@@ -93,7 +93,7 @@ module.exports = function(db) {
 		resave: true,
 		secret: config.sessionSecret,
 		store: new mongoStore({
-			db: db.connection.db,
+			mongooseConnection: db.connection,
 			collection: config.sessionCollection
 		})
 	}));
