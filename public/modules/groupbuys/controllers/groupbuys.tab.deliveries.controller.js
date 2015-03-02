aa65fced6398b5c58433f503e1ac5911e45038d8
Chema <chemaur@gmail.com>
2015-03-02 12:44:34 +0100

    Deliveries tab initial commit - view - controller

--- public/modules/groupbuys/controllers/groupbuys.tab.deliveries.controller.js
+++ public/modules/groupbuys/controllers/groupbuys.tab.deliveries.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
 // Deliveries tab controller
-angular.module('groupbuys').controller('GroupbuysTabDeliveriesController', ['$scope', '$stateParams', '$location', '$translate', 'Authentication', 'Groupbuys',
-  function($scope, $stateParams, $location, $translate, Authentication, Groupbuys) {
+angular.module('groupbuys').controller('GroupbuysTabDeliveriesController', ['$scope', 'Restangular', '$stateParams', '$location', '$translate', 'Authentication', 'Groupbuys',
+  function($scope, Restangular, $stateParams, $location, $translate, Authentication, Groupbuys) {
+
     $scope.authentication = Authentication;
+
+
+    $scope.loadDeliveriesData = function(){
+        // TODO Donde poner esto?
+        $scope.authentication.user.deliveryAddress = '';
+    };
+
+
+    $scope.copyHomeAdress = function(){
+        // TODO Cambiar el scope.user.deliveryAddress
+        $scope.authentication.user.deliveryAddress = $scope.authentication.user.homeAddress;
+    };
+
+
  }
 ]);
