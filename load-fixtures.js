4b15fb77cbdae9cccababb1b9f974d8aa7486d6c
Luis Ig. Bacas Riveiro <djblue.es@gmail.com>
2015-04-07 13:38:50 +0200

    Bug #51: Create fixtures for users, group buys and items in development environment.

--- load-fixtures.js
+++ load-fixtures.js
@@ -9,7 +9,13 @@ var init     = require('./config/init')(),
     fs       = require('fs'),
     path     = require('path'),
     async    = require('async'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    _ 	     = require('lodash');
+
+var idCurrencies = {},
+    idUsers      = {},
+    idGroupbuys  = {},
+    idItems      = {};
 
 
 // Bootstrap db connection
@@ -36,25 +42,44 @@ var loadCurrencies = function(currenciesCallback) {
         currency,
         length = currencies.length;
 
+    console.log(chalk.green(' * Loading currencies...'));
+
     // 1st para in async.each() is the array of items
     async.each(currencies,
     // 2nd param is the function that each item is passed to
     function(currency, callback) {
         currency = new Currency(currency);
 
-        currency.save(function(err, currency) {
+        currency.save(function(err) {
             if (err) {
-        		console.log(chalk.red(err));
-            } else {
-                console.log ('    ' + currency.name);
-            }
+                if (err.message.indexOf('11000 E11000 duplicate key error') !== -1) {
+                    // If the items exits get the id
+                    Currency.findOne({code: currency.code}, 'id code name', function(err, currency) {
+                        idCurrencies[currency.code] = currency.id;
+                        console.log (chalk.blue('    >>> ' + currency.name));
 
-            // Async call is done, alert via callback
-            callback();
+                        // Async call is done, alert via callback
+                        callback();
+                    });
+                } else {
+                    console.log(chalk.red(err));
+
+                    // Async call is done, alert via callback
+                    callback();
+                }
+            } else {
+                idCurrencies[currency.code] = currency.id;
+                console.log ('    ' + currency.name);
+
+                // Async call is done, alert via callback
+                callback();
+            }
         });
     },
     // 3rd param is the function to call when everything's done
     function(err) {
+        console.log(chalk.green(' * Currencies fixtures loaded.'));
+
         if (currenciesCallback) {
             currenciesCallback();
         }
@@ -63,15 +88,204 @@ var loadCurrencies = function(currenciesCallback) {
 
 
 /*
+ * Load user fixtures in DB
+ */
+var loadUsers = function(usersCallback) {
+    var User = mongoose.model('User'),
+        users = JSON.parse(fs.readFileSync('./fixtures/users.json', 'utf8')),
+        user,
+        length = users.length;
+
+    console.log(chalk.green(' * Loading users...'));
+
+    // 1st para in async.each() is the array of items
+    async.each(users,
+    // 2nd param is the function that each item is passed to
+    function(user, callback) {
+        user = new User(user);
+
+        user.save(function(err) {
+            if (err) {
+                if (err.message.indexOf('11000 E11000 duplicate key error') !== -1) {
+                    // If the items exits get the id
+                    User.findOne({'username': user.username}, 'id username', function(err, user) {
+                        idUsers[user.username] = user.id;
+                        console.log (chalk.blue('    >>> ' + user.username));
+
+                        // Async call is done, alert via callback
+                        callback();
+                    });
+                } else {
+                    console.log(chalk.red(err));
+
+                    // Async call is done, alert via callback
+                    callback();
+                }
+            } else {
+                idUsers[user.username] = user.id;
+                console.log ('    ' + user.username);
+
+                // Async call is done, alert via callback
+                callback();
+            }
+        });
+    },
+    // 3rd param is the function to call when everything's done
+    function(err) {
+        console.log(chalk.green(' * Users fixtures loaded.'));
+
+        if (usersCallback) {
+            usersCallback();
+        }
+    });
+};
+
+
+/*
+ * Load groupbuys fixtures in DB
+ */
+var loadGroupbuys = function(groupbuysCallback) {
+    var Groupbuy = mongoose.model('Groupbuy'),
+        groupbuys = JSON.parse(fs.readFileSync('./fixtures/groupbuys.json', 'utf8')),
+        groupbuy,
+        length = groupbuys.length;
+
+    console.log(chalk.green(' * Loading groupbuys...'));
+
+    // 1st para in async.each() is the array of items
+    async.each(groupbuys,
+    // 2nd param is the function that each item is passed to
+    function(groupbuy, callback) {
+        // Replace usernames by ids
+        groupbuy.managers = _.map(groupbuy.managers, function(username) {
+            return idUsers[username];
+        });
+        groupbuy.members = _.map(groupbuy.members, function(username) {
+            return idUsers[username];
+        });
+        groupbuy.user = idUsers[groupbuy.user];
+
+        // Replace currencies by ids
+        groupbuy.currencies.local = idCurrencies[groupbuy.currencies.local];
+        groupbuy.currencies.provider = idCurrencies[groupbuy.currencies.provider];
+
+        // Find if groupbuy exists
+        Groupbuy.findOne({name: groupbuy.name}, 'id name title', function(err, groupbuy2) {
+            if (!err && groupbuy2) {
+                idGroupbuys[groupbuy2.name] = groupbuy2.id;
+                console.log (chalk.blue('    >>> ' + groupbuy2.title));
+
+                // Async call is done, alert via callback
+                callback();
+            } else {
+                // Save groupbuy
+                groupbuy = new Groupbuy(groupbuy);
+
+                groupbuy.save(function(err) {
+                    if (err) {
+                        console.log(chalk.red(err));
+                    } else {
+                        idGroupbuys[groupbuys.name] = groupbuys.id;
+                        console.log ('    ' + groupbuy.title);
+                    }
+
+                    // Async call is done, alert via callback
+                    callback();
+                });
+            }
+        });
+    },
+    // 3rd param is the function to call when everything's done
+    function(err) {
+        console.log(chalk.green(' * Groupbuys fixtures loaded.'));
+
+        if (groupbuysCallback) {
+            groupbuysCallback();
+        }
+    });
+};
+
+
+/*
+ * Load groupbuys fixtures in DB
+ */
+var loadItems = function(itemsCallback) {
+    var Item = mongoose.model('Item'),
+        items = JSON.parse(fs.readFileSync('./fixtures/items.json', 'utf8')),
+        item,
+        length = items.length;
+
+    console.log(chalk.green(' * Loading items...'));
+
+    // 1st para in async.each() is the array of items
+    async.each(items,
+    // 2nd param is the function that each item is passed to
+    function(item, callback) {
+        // Replace by IDs
+        item.user     = idUsers[item.user];
+        item.currency = idCurrencies[item.currency];
+        item.groupbuy = idGroupbuys[item.groupbuy];
+
+        // Find if groupbuy exists
+        Item.findOne({title: item.title, groupbuy: item.groupbuy}, 'id title', function(err, item2) {
+            if (!err && item2) {
+                idItems[item2.title] = item2.id;
+                console.log (chalk.blue('    >>> ' + item2.title));
+
+                // Async call is done, alert via callback
+                callback();
+            } else {
+                // Save groupbuy
+                item = new Item(item);
+
+                item.save(function(err) {
+                    if (err) {
+                        console.log(chalk.red(err));
+                    } else {
+                        idItems[item.title] = item.id;
+                        console.log ('    ' + item.title);
+                    }
+
+                    // Async call is done, alert via callback
+                    callback();
+                });
+            }
+        });
+    },
+    // 3rd param is the function to call when everything's done
+    function(err) {
+        console.log(chalk.green(' * Items fixtures loaded.'));
+
+        if (itemsCallback) {
+            itemsCallback();
+        }
+    });
+};
+
+
+
+/*
  * Main code
  */
 console.log(chalk.green('Start Loading fixtures!'));
 
-console.log(chalk.green(' * Loading currencies...'));
-loadCurrencies(function() {
-    console.log(chalk.green(' * Currencies fixtures loaded.'));
 
-    // Exit
-    console.log(chalk.green('- End fixtures -'));
-    process.exit();
+loadCurrencies(function(err) {
+    if (err) console.log(chalk.red(err));
+
+    loadUsers(function(err) {
+        if (err) console.log(chalk.red(err));
+
+        loadGroupbuys(function(err) {
+            if (err) console.log(chalk.red(err));
+
+            loadItems(function(err) {
+                if (err) console.log(chalk.red(err));
+
+                // Exit
+                console.log(chalk.green('- End fixtures -'));
+                process.exit();
+            });
+        });
+    });
 });
