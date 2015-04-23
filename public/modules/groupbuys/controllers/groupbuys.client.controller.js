aeb82d5700f9856675f61b9a55b101449579d067
Chema <chemaur@gmail.com>
2015-04-23 19:44:43 +0200

    Do not show request-resume tab

--- public/modules/groupbuys/controllers/groupbuys.client.controller.js
+++ public/modules/groupbuys/controllers/groupbuys.client.controller.js
@@ -263,9 +263,6 @@ function($scope, Restangular, $stateParams, $location, $translate, Authenticatio
 								title: translations['groupbuys.Requests'],
 								template:'/modules/groupbuys/views/tabs/requests-groupbuy.client.view.html'
 							},{
-								title: translations['groupbuys.Requests_summary'],
-								template:'/modules/groupbuys/views/tabs/requests-summary-groupbuy.client.view.html'
-							},{
 								title: translations['groupbuys.Messaging'],
 								template:'/modules/groupbuys/views/tabs/messaging-groupbuy.client.view.html'
 							},{
